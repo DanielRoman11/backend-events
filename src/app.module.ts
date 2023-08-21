c2a972e29c41920ccffd231412105dd0306e0523
danielroman11 <danielroman.05111@gmail.com>
2023-08-21 17:00:28 -0500

    addon: Conection on using DOCKER and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'password',
+    database: 'nest-events'
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
