d4a0459795c1cd14aab9be6574c2caebd1375774
Daniel Román <danielroman.05111@gmail.com>
2023-12-31 04:05:15 -0500

    feat(current-User) creating a simple User decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Injectable, Logger, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
+import { CurrentUser } from "./current-user.decorator";
+import { User } from "./user.entity";
 
 @Controller('auth')
 export class AuthController {
@@ -12,16 +14,16 @@ export class AuthController {
 
   @Post('login')
   @UseGuards(AuthGuard('local'))
-  async login(@Request() req) {
+  async login(@CurrentUser() user: User) {
     return {
-      userId: req.user.id,
-      token: this.authService.getTokenForUser(req.user),
+      userId: user.id,
+      token: this.authService.getTokenForUser(user)
     }
   }
 
   @Get('profile')
   @UseGuards(AuthGuard('jwt'))
-  async getProfile(@Request() req){
-    return req.user;
+  async getProfile(@CurrentUser() user: User){
+    return user;
   }
 }
