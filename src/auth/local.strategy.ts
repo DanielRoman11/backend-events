eef3d90d4cd3635bfa18fcc6b690486dd5684ee9
Daniel Román <danielroman.05111@gmail.com>
2023-12-31 01:03:38 -0500

    feat(auth) loca.strategy

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable, Logger, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-local";
+import { User } from "./user.entity";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  private readonly logger = new Logger(LocalStrategy.name);
+  
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) { super() }
+
+  public async validate(username: string, password: string): Promise<any>{
+    const user = await this.userRepository.findOne({
+      where: { username }
+    });
+
+    if(!user){
+      this.logger.debug(`User ${username} not found!`);
+      throw new UnauthorizedException();
+    }
+
+    if(password !== user.password){
+      this.logger.debug(`Invalid credentials for user ${username}`);
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
