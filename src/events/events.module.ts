d528a4b722f815d905c2836db6fd6cc10f78a015
Daniel Román <danielroman.05111@gmail.com>
2024-01-05 01:12:47 -0500

    feat(event-by-user) new Service method getByUser

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -6,12 +6,14 @@ import { Attendee } from './attendee.entity';
 import { ExampleController } from './example.Controller';
 import { EventsService } from './event.service';
 import { AttendeesService } from './attendee.service';
+import { EventAttendeeController } from './event-attendee.controller';
+import { EventsOrganizedByUserController } from './events-by-user.Controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Event, Attendee])
   ],
-  controllers: [EventsController, ExampleController],
+  controllers: [EventsController, ExampleController, EventAttendeeController, EventsOrganizedByUserController],
   providers: [EventsService, AttendeesService]
 })
 export class EventsModule {}
