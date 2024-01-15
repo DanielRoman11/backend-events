7031ceb5372bec68774e22383df276d0170e11c3
Daniel Román <danielroman.05111@gmail.com>
2024-01-15 01:32:30 -0500

    fix(Event-Attendee): Using deafult value for ParseIntPipe Validation

--- src/events/event-attendee.controller.ts
+++ src/events/event-attendee.controller.ts
@@ -1,8 +1,8 @@
-import { ClassSerializerInterceptor, Controller, Get, Param, ParseIntPipe, SerializeOptions, UseInterceptors } from "@nestjs/common";
+import { ClassSerializerInterceptor, Controller, DefaultValuePipe, Get, Param, ParseIntPipe, SerializeOptions, UseInterceptors } from "@nestjs/common";
 import { AttendeesService } from "./attendee.service";
 import { Attendee } from "./attendee.entity";
 
-@Controller('events/:eventId/Attendees')
+@Controller('events/:eventid/attendees')
 @SerializeOptions({ strategy: 'excludeAll' })
 export class EventAttendeeController{
   constructor(
@@ -14,7 +14,7 @@ export class EventAttendeeController{
    */
   @Get()
   @UseInterceptors(ClassSerializerInterceptor)
-  public async getAll(@Param('eventId', new ParseIntPipe()) eventId: number): Promise<Attendee> {
-    return await this.attendeesService.findByEventId(eventId)
+  public async getAll(@Param('eventId', new DefaultValuePipe(1), new ParseIntPipe()) eventId: number): Promise<Attendee[]> {
+    return await this.attendeesService.findByEventId(eventId);
   }
 }
