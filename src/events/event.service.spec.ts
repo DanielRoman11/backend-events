421c931ba24ce7971ed1e45367cd171eeb6ef08f
Daniel Román <danielroman.05111@gmail.com>
2024-02-28 18:51:17 -0500

    feat(EventService): Creating a Mock repo for Testing

--- src/events/event.service.spec.ts
+++ src/events/event.service.spec.ts
@@ -1,3 +1,31 @@
+import { Repository, createQueryBuilder } from "typeorm";
+import { EventsService } from "./event.service"
+import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
 describe('EventService', ()=>{
-  
+  let service: EventsService;
+  let repository: Repository<Event>
+
+  beforeEach(async ()=>{
+    const module = await Test.createTestingModule({
+      providers: [
+        EventsService,
+        {
+          provide: getRepositoryToken,
+          useValue: {
+            save: jest.fn(),
+            createQueryBuilder: jest.fn(),
+            delete: jest.fn(),
+            where: jest.fn(),
+            execute: jest.fn(),
+          }
+        }
+      ]
+    }).compile();
+
+    service = module.get<EventsService>(EventsService)
+    repository = module
+      .get<Repository<Event>>(getRepositoryToken(Event))
+  });
 })
