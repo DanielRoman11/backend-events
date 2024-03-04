96cccf10b590b1f303cd980ad6a5d7fe157c0c14
Daniel Román <danielroman.05111@gmail.com>
2024-03-04 00:09:24 -0500

    bug(EventService) No se esta estableciendo bien el UpdateEventDto en el test

--- src/events/event.service.spec.ts
+++ src/events/event.service.spec.ts
@@ -2,6 +2,7 @@ import { Repository, createQueryBuilder } from "typeorm";
 import { EventsService } from "./event.service"
 import { Test } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
+import { Event } from "./event.entity";
 
 describe('EventService', ()=>{
   let service: EventsService;
@@ -26,6 +27,18 @@ describe('EventService', ()=>{
 
     service = module.get<EventsService>(EventsService)
     repository = module
-      .get<Repository<Event>>(getRepositoryToken(Event))
+      .get<Repository<Event>>(
+        getRepositoryToken(Event)
+      )
   });
-})
+
+  describe('updateEvent', ()=>{
+    it('should update an event', ()=>{
+      const repoSpy = jest.spyOn(repository, 'save')
+        .mockResolvedValue({id: 1} as Event);
+      
+      expect(service.updateEvent(new Event({id: 1}), {id: 1})).resolves.toEqual({ id: 1})
+      expect(repoSpy).toHaveBeenCalledWith({id: 1});
+    });
+  });
+});
