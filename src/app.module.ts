da8560ddfbba9dd79309d4939be36ba7dacd64ae
Daniel Román <danielroman.05111@gmail.com>
2024-03-07 14:08:42 -0500

    changing dotenv file looking on database.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,19 @@ import { AppColombiaService } from './app.Colombia.service';
 import { AppDummy } from './app.dummy';
 import { Authmodule } from './auth/auth.module';
 import { DatabaseModule } from './config/database.module';
-
+import * as path from 'path';
 import * as dotenv from 'dotenv';
-dotenv.config();
 
-console.log(process.env.NODE_ENV);
+(async function() {
+  try {
+    const envPath = path.resolve(process.cwd(), `${process.env.NODE_ENV.trim()}.env`).replaceAll('\\', '/');
+
+    dotenv.config({ path: envPath });
+    dotenv.configDotenv({path: envPath})
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 @Module({
   imports: [
--- test/events.e2e-spec.ts
+++ test/events.e2e-spec.ts
@@ -1,15 +1,17 @@
-import { INestApplication } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
-import * as request from 'supertest';
+import { INestApplication } from "@nestjs/common";
+import { Test, TestingModule } from "@nestjs/testing";
+import * as request from "supertest";
+import { AppModule } from "./../src/app.module";
 
 let app: INestApplication;
 let mod: TestingModule;
 
+
+
 describe('Events (e2e)', () => {
   beforeAll(async () => {
     mod = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = mod.createNestApplication();
@@ -21,6 +23,8 @@ describe('Events (e2e)', () => {
   });
 
   it('should return an empty list of events', async () => {
-    return request(app.getHttpServer()).get('/events').expect(200);
+    return request(app.getHttpServer())
+      .get('/events')
+      .expect(200);
   });
 });
