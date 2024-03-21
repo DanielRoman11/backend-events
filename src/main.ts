852be2e8d3f6f47ed9c11367f703e35fc3bc996c
Daniel Román <danielroman.05111@gmail.com>
2024-03-20 21:42:35 -0500

    feat(Swagger): Routes on root endpoint

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config)
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   const port = process.env.PORT || 3000
   await app.listen(port, () =>{
